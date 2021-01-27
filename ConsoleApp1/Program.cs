99186f8077c1e3f1694466e158ada6fcee4cc7bb
shimat <shima@sansan.com>
2021-01-27 13:56:41 +0900

    Add first code

---
+++ ConsoleApp1/Program.cs
@@ -0,0 +1,19 @@
+﻿using System;
+
+namespace ConsoleApp1
+{
+    public class demo : Exception
+    {
+        public static void Initialize(int size) { }
+        protected static readonly int _item;
+        public static int item { get { return _item; } }
+    }
+
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
